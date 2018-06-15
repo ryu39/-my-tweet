e579cfdb24c8da3e7dc7fef207e6d0be50351607
Ryusuke Homma <ryusuke.homma@unifa-e.com>
2018-06-15 12:49:35 +0900

    Implements TweetsController#index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,16 @@
 class TweetsController < ApplicationController
+  before_action :user_must_be_authenticated
+
   def index
-    # TODO
+    @tweets = @user.tweets
+  end
+
+  private
+
+  def user_must_be_authenticated
+    @user = ::User.find_by(id: session[:user_id])
+    if @user.nil?
+      redirect_to new_session_path
+    end
   end
 end
