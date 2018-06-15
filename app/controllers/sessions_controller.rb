dddc4b7685af3e521a3f58c511b6950def38ca15
Ryusuke Homma <ryusuke.homma@unifa-e.com>
2018-06-15 12:32:38 +0900

    Implements SessionsController#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # TODO
+    @form = ::Sessions::NewForm.new(new_form_params)
+    if @form.invalid?
+      render :new and return
+    end
+
+    user = ::User.find_by(uid: @form.uid)
+    unless user&.authenticate(@form.password)
+      @form.errors.add(:base, :authentication_failed)
+      render :new and return
+    end
+
+    reset_session
+    session[:user_id] = user.id
+    redirect_to tweets_path
   end
 
   def destroy
     # TODO
+  end
+
+  private
+
+  def new_form_params
+    params.require(:sessions_new_form).permit(:uid, :password)
   end
 end
