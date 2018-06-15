92b3277e8037028d800d74a077bb04f1dc012034
Ryusuke Homma <ryusuke.homma@unifa-e.com>
2018-06-15 16:26:25 +0900

    Add relation from User to Doorkeeper objects

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :tweets, ->() { order(id: :desc) }
+  has_many :access_grants, class_name: "Doorkeeper::AccessGrant",
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+  has_many :access_tokens, class_name: "Doorkeeper::AccessToken",
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
 end
