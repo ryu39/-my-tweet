301dbf5569fde362003a0c64eb0f0596b6f1ec9e
Ryusuke Homma <ryusuke.homma@unifa-e.com>
2019-03-19 10:38:41 +0900

    Extract check auth logic as common method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def user_must_be_authenticated
+    @user = ::User.find_by(id: session[:user_id])
+    if @user.nil?
+      redirect_to new_session_path
+    end
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,13 +4,4 @@ class TweetsController < ApplicationController
   def index
     @tweets = @user.tweets
   end
-
-  private
-
-  def user_must_be_authenticated
-    @user = ::User.find_by(id: session[:user_id])
-    if @user.nil?
-      redirect_to new_session_path
-    end
-  end
 end
