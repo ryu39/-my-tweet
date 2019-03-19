23f67c94b1f0fb599c21b22ce12cefb487c86e73
Ryusuke Homma <ryusuke.homma@unifa-e.com>
2019-03-19 10:48:14 +0900

    Add admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_015139) do
+ActiveRecord::Schema.define(version: 2019_03_19_014636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_015139) do
     t.string "password_digest", limit: 256, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["uid"], name: "index_users_on_uid"
   end
 
